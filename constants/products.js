c9d3812fc18029e7bea42e6fd29da02b2439d150
adolffau60 <adolffaustine200@gmail.com>
2024-05-09 07:38:23 +0300

    main

---
+++ constants/products.js
@@ -0,0 +1,30 @@
+
+export default [
+  {
+    title: 'Rhino cement...',
+    image: 'https://dreamcivil.com/wp-content/uploads/2021/09/1683130292023-696x479.jpg.webp',
+    price: 40000,
+    horizontal: true,
+  },
+  {
+    title: 'Timbers',
+    image: 'https://dreamcivil.com/wp-content/uploads/2021/09/timber-as-construction-material-min-300x171.jpg.webp',
+    price: 200000,
+  },
+  {
+    title: 'Tiles',
+    image: 'https://dreamcivil.com/wp-content/uploads/2021/07/broken_tiles_xbrokencropped-min-e1627271824438-238x300.jpg.webp',
+    price: 40000,
+  },
+  {
+    title: 'Gravel',
+    image: 'https://dreamcivil.com/wp-content/uploads/2021/09/stone-3331916_1280-1024x682-1-300x200.jpg',
+    price: 88000,
+    horizontal: true,
+  },
+  {
+    title: 'Iron',
+    image: 'https://dreamcivil.com/wp-content/uploads/2021/09/90109530-modern-steel-roof-construction-iron-work-construction-tin-roofs-skylights-in-roof-ceiling-constructi-min-300x200.jpg',
+    price: 800000,
+  },
+];
